b7aa43a1f11c19dd8af1ac7a686c1e701f164f51
Don Petersen <don@donpetersen.net>
2016-11-04 16:42:47 -0700

    Fix issue found in some dumps.
    See link in the comment.

--- import-db.sh
+++ import-db.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
+# sed comes from and is explained here:
+# http://stackoverflow.com/questions/9446783/remove-definer-clause-from-mysql-dumps
+
 aws s3 cp $SOURCE_S3_URL - | \
   gunzip | \
+  sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/' | \
   mysql --user=$TARGET_USERNAME \
         --password=$TARGET_PASSWORD \
         --host=$TARGET_HOST \
